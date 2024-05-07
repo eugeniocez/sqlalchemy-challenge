1a9238d5edb62633dece7c4a50acdca0575bf6d9
eugeniocez <eugenio.cez@gmail.com>
2024-05-07 09:34:33 -0600

    Updated app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -62,3 +62,65 @@ def precipitation():
         precipitation_data = {row.date: row.prcp for row in result}
 
     return jsonify(precipitation_data)
+
+@app.route("/api/v1.0/stations")
+def stations():
+
+    query = text("SELECT station FROM station")
+    with Session(engine) as session:
+        result = session.execute(query)
+        stations = [row.station for row in result]
+    return jsonify(stations)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+    most_active_id = 'USC00519281'
+    
+
+    with Session(engine) as session:
+    
+        most_recent_date = session.query(func.max(Measurement.date)).scalar()
+    
+        lastdate_in_dataset = dt.datetime.strptime(most_recent_date, '%Y-%m-%d')
+        one_year_ago_date = lastdate_in_dataset - dt.timedelta(days=366)
+        
+        station_query = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == most_active_id).filter(Measurement.date >= one_year_ago_date).all()
+        tobs_data = {row.date: row.tobs for row in station_query}
+        
+    return jsonify(tobs_data)
+
+@app.route("/api/v1.0/<start>")
+def start(start):
+    
+
+    with Session(engine) as session:
+
+        start_date = dt.datetime.strptime(start, "%Y-%m-%d").date()
+ 
+        result =session.query(func.min(Measurement.tobs), 
+                              func.avg(Measurement.tobs), 
+                              func.max(Measurement.tobs)).filter(Measurement.date >= start_date).all()
+        temps = list(result[0])
+    
+    return jsonify(temps)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+
+    with Session(engine) as session:
+
+
+        start_date = dt.datetime.strptime(start, "%Y-%m-%d").date()
+        end_date = dt.datetime.strptime(end, "%Y-%m-%d").date()
+
+        result =session.query(func.min(Measurement.tobs), 
+                              func.avg(Measurement.tobs), 
+                              func.max(Measurement.tobs)).filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+        temps = list(result[0])
+
+    return jsonify(temps)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
